4174fd41beb31bbc07d33cedc1935353f4b37334
Lawrence Bird <lawrence.bird@hamburg.de>
2019-08-14 16:14:45 +0200

    embedded reviews in show

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,11 +3,6 @@ class ReviewsController < ApplicationController
     @reviews = Review.all
   end
 
-  def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
-    @review = Review.new
-  end
-
   def create
     @review = Review.new(review_params)
     @restaurant = Restaurant.find(params[:restaurant_id])
